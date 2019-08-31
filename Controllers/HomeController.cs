26913c4d1fc0e1a879b979bd036b526fdcd6a5c7
Michael Friis <friism@gmail.com>
2019-08-31 22:21:46 +0400

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_on_lyra.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        [Route("")]
+        public IActionResult Test()
+        {
+            Console.WriteLine($"I got something at {DateTime.Now:HH:mm:ss}");
+
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = reader.ReadToEnd();
+
+                Console.WriteLine(body);
+            }
+            return new EmptyResult();
+        }
+    }
+}
